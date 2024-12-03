8ffb85763d99cf51538fa1607a9160677bbb43c7
mariaromerogodoy <Romem899@newschool.edu>
2024-12-03 13:52:42 -0500

    push

---
+++ WHack a MoLE/script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    setGame();
+  }
+  
+  function setGame() {
+    //set up the grid for the game board in html
+    for (let i = 0; i < 9; i++) { //i goes from 0 to 8, stops at 9
+      //<div id="0-8"></div>
+      let tile = document.createElement("div");
+      title.id = i.toString();
+      document.getElementById("board").appendChild(title);
+    }
+  }
