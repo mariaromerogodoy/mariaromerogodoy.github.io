e88cd5e94959b22fcb8677b8d6ef46e4398e6da8
mariaromerogodoy <Romem899@newschool.edu>
2024-12-03 14:57:19 -0500

    h

--- WHack a MoLE/script.js
+++ WHack a MoLE/script.js
@@ -10,4 +10,20 @@ window.onload = function() {
       title.id = i.toString();
       document.getElementById("board").appendChild(tile);
     }
+    setInterval(setMole, 2000); //2000 milliseconds = 2 seconds
+  }
+
+
+function getRandomTile() {
+    //math.random --> (0-1) * 9 = (0-9) --> round down to (0-8) integers
+    let num = Math.floor(Math.random() * 9)
+    return num.toString();
+}
+  function setMole() {
+    let mole = document.createElement("img");
+    mole.src= "./mole.png";
+
+    let num = getRandomTile();
+    currMoleTile = document.getElementById(num);
+    currMoleTile.appendChild(mole);
   }
