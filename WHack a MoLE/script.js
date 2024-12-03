7b4af269097a1ed4be3fe5e453c545f7ffa37b31
mariaromerogodoy <Romem899@newschool.edu>
2024-12-03 15:03:05 -0500

    push

--- WHack a MoLE/script.js
+++ WHack a MoLE/script.js
@@ -7,7 +7,7 @@ window.onload = function() {
     for (let i = 0; i < 9; i++) { //i goes from 0 to 8, stops at 9
       //<div id="0-8"></div>
       let tile = document.createElement("div");
-      title.id = i.toString();
+      tile.id = i.toString();
       document.getElementById("board").appendChild(tile);
     }
     setInterval(setMole, 2000); //2000 milliseconds = 2 seconds
@@ -15,13 +15,13 @@ window.onload = function() {
 
 
 function getRandomTile() {
-    //math.random --> (0-1) * 9 = (0-9) --> round down to (0-8) integers
-    let num = Math.floor(Math.random() * 9)
+    // math.random --> (0-1) * 9 = (0-9) --> round down to (0-8) integers
+    let num = Math.floor(Math.random() * 9);
     return num.toString();
 }
   function setMole() {
     let mole = document.createElement("img");
-    mole.src= "./mole.png";
+    mole.src = "./mole.png";
 
     let num = getRandomTile();
     currMoleTile = document.getElementById(num);
