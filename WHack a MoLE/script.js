33de5ffdc890f79febac190e5f72b06c97af9a60
mariaromerogodoy <Romem899@newschool.edu>
2024-12-03 20:18:47 -0500

    r

--- WHack a MoLE/script.js
+++ WHack a MoLE/script.js
@@ -2,14 +2,17 @@ let currMoleTile;
 let currPlantTile;
 
 
-
- 
+window.onload = function() {
+    setGame();
+  }
   
   function setGame() {
     //set up the grid for the game board in html
     for (let i = 0; i < 9; i++) { //i goes from 0 to 8, stops at 9
       //<div id="0-8"></div>
-      
+      let tile = document.createElement("div");
+      tile.id = i.toString();
+      document.getElementById("board").appendChild(tile);
     }
 
     setInterval(setMole, 1000); //1000 milliseconds = 1 seconds
